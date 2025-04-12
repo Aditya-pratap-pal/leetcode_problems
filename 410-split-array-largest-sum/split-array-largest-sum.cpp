3ba4b61ed8b6909de63f7701cb56a85afec98277
Aditya-pratap-pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2025-04-12 11:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (26.21%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -5,15 +5,15 @@ public:
         for (int i = 0; i < v.size(); i++) {
             if (v[i] > mid)
                 return true;
-            sum += v[i];
-            if (sum> mid) {
+            if (sum+v[i] > mid) {
                 par++;
-                sum = v[i];
+                // sum = v[i];
+                sum=0;
             }
-            if (par > k)
-                return true;
+            sum += v[i];
         }
-
+        if (par > k)
+            return true;
         return false;
     }
     int splitArray(vector<int>& nums, int k) {
