c28b65652afb84bcee4ac944ea3276475720ca21
Aditya-pratap-pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2025-09-24 01:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (8.06%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> func(TreeNode* root){
+        // temp.push_back(root->val);
+        vector<vector<int>> v;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            vector<int> temp;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode* cNode=q.front();
+                q.pop();
+                if(cNode!=nullptr)temp.push_back(cNode->val);
+                if(cNode!=nullptr&&cNode->left!=nullptr) q.push(cNode->left);
+                if(cNode!=nullptr&&cNode->right!=nullptr) q.push(cNode->right);
+            }
+            if(temp.size()!=0)v.push_back(temp);
+        }
+        return v;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<vector<int>> ans=func(root);
+        vector<int> v;
+        for(auto it:ans){
+            v.push_back(it[it.size()-1]);
+        }
+        return v;
+    }
+};
