66d80ec30dff96f48745cc99b045eeb6f92df176
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-02-05 23:51:23 +0530

    Time: 14 ms (35.97%) | Memory: 17.5 MB (38.96%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,12 +9,33 @@
  */
 class Solution {
 public:
+// bool flag=true;
+//     TreeNode* func(TreeNode* root,TeeNode* p,TreeNode* q,TreeNode* &ans){
+//         if(root==nullptr)return ;
+//         if(root==p)return ;
+//         if(root==q)return ;
+//         lowestCommonAncestor(root->left,p,q);
+//         lowestCommonAncestor(root->right,p,q);
+//         if(left==nullptr && right==nullptr)return ;
+//         if(left!=nullptr && right!=nullptr && flag){
+//             flag=false;
+//             ans=root;
+//         }
+//         if(left!=nullptr){
+//             return;
+//         }
+//         return;
+
+//     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root==q || root==nullptr)return root;
+        if(root==nullptr)return root;
+        if(root==p)return root;
+        if(root==q)return root;
         TreeNode* left = lowestCommonAncestor(root->left,p,q);
         TreeNode* right = lowestCommonAncestor(root->right,p,q);
-        if(right==nullptr)return left;
+        if(left==nullptr && right ==nullptr)return nullptr;
         else if(left==nullptr)return right;
-        else return root;
+        else if(right==nullptr)return left;
+        return root;
     }
 };
