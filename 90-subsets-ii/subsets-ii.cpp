bbd1267f000afce0a49a13ad48180b43cddc5c92
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-02-08 16:51:35 +0530

    Time: 6 ms (8.66%) | Memory: 17.1 MB (6.57%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -11,12 +11,13 @@ void func(vector<int> v,int n,int i,vector<int>& curr,vector<vector<int>> &ans){
         func(v,n,i+1,curr,ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans,final;
         vector<int> curr;
         func(nums,nums.size(),0,curr,ans);
         set<vector<int>> s;
         for(int i=0;i<ans.size();i++){
-            sort(ans[i].begin(),ans[i].end());
+            // sort(ans[i].begin(),ans[i].end());
             s.insert(ans[i]);
         }
         for(auto it:s){
