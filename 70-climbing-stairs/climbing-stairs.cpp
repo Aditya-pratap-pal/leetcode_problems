df9c4a17606b15cc809b2683847fb7c82cc85813
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-02-22 22:50:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
 vector<int> dp;
-int step(int n){
-    if(n==1||n==0)return dp[n]=1;
-    if(dp[n]!=-1)return dp[n];
-    return dp[n]=step(n-1)+step(n-2);
-}
+// int step(int n){
+//     if(n==1||n==0)return dp[n]=1;
+//     if(dp[n]!=-1)return dp[n];
+//     return dp[n]=step(n-1)+step(n-2);
+// }
     int climbStairs(int n) {
+        // dp.resize(n+1,-1);
+        // return step(n);
         dp.resize(n+1,-1);
-        return step(n);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
