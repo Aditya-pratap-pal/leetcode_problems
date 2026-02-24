d0a6e704266cad5988453da96c664f3ad2b2ebe8
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-02-24 11:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.45%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,9 +9,23 @@ public:
         return dp[i] = max(next,nextToNext);
     }
     int rob(vector<int>& nums) {
-        dp.resize(nums.size()+2,-1);
-        int idx1 = money(nums,nums.size(),0);
-        int idx2 = money(nums,nums.size(),1);
-        return max(idx1,idx2);
+        // dp.resize(nums.size()+2,-1);
+        // int idx1 = money(nums,nums.size(),0);
+        // int idx2 = money(nums,nums.size(),1);
+        // return max(idx1,idx2);
+
+        //Tabulation---------
+        int n = nums.size();
+        if(n==1)return nums[0];
+        else if(n==2)return max(nums[0],nums[1]);
+        else if(n==3)return max(nums[0]+nums[2],nums[1]);
+        vector<int> dp(n);
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=nums[2]+nums[0];
+        for(int i=3;i<n;i++){
+            dp[i]=nums[i]+max(dp[i-2],dp[i-3]);
+        }
+        return max(dp[n-1],dp[n-2]);
     }
 };
