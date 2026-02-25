b4df3580d9f31e780b5a25834421c4d9c12fb22f
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-02-25 23:48:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (38.10%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -10,15 +10,37 @@ vector<int> dp;
         return dp[i] = max(one,two);
     }
     int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // if(n==1) return nums[0];
+
+        // dp.assign(n+2,-1);
+        // int one = money(nums,n-1,0);
+
+        // dp.assign(n+2,-1);
+        // int two = money(nums,n,1);
+
+        // return max(one,two);
         int n = nums.size();
-        if(n==1) return nums[0];
-
-        dp.assign(n+2,-1);
-        int one = money(nums,n-1,0);
-
-        dp.assign(n+2,-1);
-        int two = money(nums,n,1);
-
-        return max(one,two);
+        vector<int> dp1(n-1,0);
+        vector<int> dp2(n,0);
+        if(n==1)return nums[0];
+        else if(n==2)return max(nums[0],nums[1]);
+        else if(n==3)return max({nums[0],nums[1],nums[2]});
+        dp1[0]=nums[0];
+        dp1[1]=nums[1];
+        dp1[2]=nums[2]+nums[0];
+        // dp2[0]=nums[0];
+        dp2[1]=nums[1];
+        dp2[2]=nums[2];
+        dp2[3]=nums[1]+nums[3];
+        for(int i=3;i<n-1;i++){
+            dp1[i]=nums[i]+max(dp1[i-2],dp1[i-3]);
+        }
+        dp1[n-2]=max(dp1[n-2],dp1[n-3]);
+        for(int i=4;i<n;i++){
+            dp2[i]=nums[i]+max(dp2[i-2],dp2[i-3]);
+        }
+        dp2[n-1]=max(dp2[n-1],dp2[n-2]);
+        return max(dp1[n-2],dp2[n-1]);
     }
 };
