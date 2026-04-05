4579dbd3e6bf6950b0837fcc7029af174d96f3b6
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-04-05 17:02:18 +0530

    Time: 47 ms (92.35%) | Memory: 111.2 MB (18.18%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool solve(vector<int> v,int n,int i,int target){
-        if(target==0)return true;
-        if(i>=n)return false;
-        if(dp[i][target]!=-1)return dp[i][target];
-        if(v[i]>target){
-            return dp[i][target] = solve(v,n,i+1,target);
-        }
-        return dp[i][target] = solve(v,n,i+1,target-v[i])||solve(v,n,i+1,target);
-    }
+vector<vector<int>> dp;
+bool func(vector<int> &v,int n,int totalsum,int sum,int idx){
+    if(sum>totalsum)return false;
+    if(sum==totalsum)return dp[idx][sum] = true;
+    if(idx>=n || sum>totalsum)return dp[idx][sum] = false;
+    if(dp[idx][sum]!=-1)return dp[idx][sum];
+    return dp[idx][sum] = func(v,n,totalsum,sum+v[idx],idx+1)||func(v,n,totalsum,sum,idx+1);
+}
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        dp.resize(n,vector<int>((sum/2)+1,0));
-        // if(sum%2==0)return solve(nums,n,0,sum/2);
-        // else return false;
-        if(sum&1)return false;
-        sum=sum/2;
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
-        if(nums[0]<=sum)dp[0][nums[0]]=1;
-        else return 0;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=sum;j++){
-                if(nums[i]>j){
-                    dp[i][j]=dp[i-1][j];
-                }
-                else{
-                    dp[i][j]= dp[i-1][j-nums[i]] || dp[i-1][j];
-                }
-            }
-        }
-        return dp[n-1][sum];
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int target=sum/2;
+        if(sum%2!=0)return false;
+        dp.resize(nums.size()+1,vector<int>(target+1,-1));
+        
+        return func(nums,nums.size(),target,0,0);
     }
 };
