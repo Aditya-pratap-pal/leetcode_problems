0c575428c7c08efea08a02d75c7d07eb3f05b166
Aditya Pratap Pal <146539756+Aditya-pratap-pal@users.noreply.github.com>
2026-07-10 14:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (26.75%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans;
-        int total=1;
-        int zero=0;
+        int n = nums.size();
+        vector<int> ans(n,1);
+        vector<int> prefix(n),suffix(n);
+        prefix[0]=nums[0];
+        suffix[n-1]=nums[n-1];
+        for(int i=1;i<nums.size();i++){
+            prefix[i]=prefix[i-1]*nums[i];
+        }
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i];
+        }
         for(int i=0;i<n;i++){
-            if(nums[i]==0)zero++;
-            else total=total*nums[i];
-        }
-        if(zero>1){
-            ans.resize(n,0);
-            return ans;
-        }
-        if(zero==1){
-            for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                ans.push_back(total);
-            }
-            else{
-                ans.push_back(0);
-            }
-            }
-        }
-        else{
-        for(int i=0;i<n;i++){
-            ans.push_back((total/nums[i]));
-        }
+            if((i-1)>=0)ans[i]*=prefix[i-1];
+            if((i+1)<n)ans[i]*=suffix[i+1];
         }
         return ans;
     }
